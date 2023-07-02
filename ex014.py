786f604b3fc6710f62ca902c116d959a30162de6
edilsonguti <131001164+edilsonguti@users.noreply.github.com>
2023-07-01 21:42:12 -0300

    ex014

---
+++ ex014.py
@@ -0,0 +1,3 @@
+c = float (input('Digite aqui a temperatura ambiente em Grau Celsius:'))
+f = ((9*c)/5)+32
+print('A temperatura atual é de {:.2f}ºCelsius que equivale a {:.2f}ºFahrenheit'.format(c, f))
