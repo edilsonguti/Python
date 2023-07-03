94c88be73724046173990c5709a5e2e2ced95005
edilsonguti <131001164+edilsonguti@users.noreply.github.com>
2023-07-03 16:21:28 -0300

    aula09

---
+++ aula09.py
@@ -0,0 +1,28 @@
+frase = 'Curso em Vídeo Python'
+print(frase)
+print(frase[4])
+print(frase[3:13])
+print(frase[:15])
+print(frase[1:15:2])
+print(frase[::2])
+print(""" Deve ser usado quando tem um texto muito grande para ser impresso, deve ser usado 3 aspas duplas no inicio e no vinal entre os parentes""")
+print(frase.count('o'))
+print(frase.upper().count('O'))
+print(len(frase))
+print(len(frase.strip()))
+print(frase.replace('Python', 'Android'))
+print(frase[0])
+frase = frase.replace('Python', 'Android')
+print(frase)
+print('Curso' in frase)
+print(frase.find('Vídeo'))
+print(frase.find('vídeo'))
+print(frase.lower().find('vídeo'))
+frase = 'Curso em Vídeo Python'
+print(frase.split())
+frase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(dividido[0])
+print(dividido[2] [3])
+
+
